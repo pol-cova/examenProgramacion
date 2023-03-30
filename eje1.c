a41541bfa772feb1f36e80577535594f480ebeaa
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-03-30 14:14:51 -0600

    First commit

---
+++ eje1.c
@@ -0,0 +1,9 @@
+//
+// Created by Paul Contreras on 30/03/23.
+//
+/*
+ * Exam exercise
+ */
+
+
+
---
+++ main.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+// Conjunto de lenguaje Sintaxis
+int main() {
+    printf("Hello, World!\n");
+    return 0;
+}
