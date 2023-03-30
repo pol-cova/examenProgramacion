f0fce4f64929a2189dce92432f8a3269c0a8faeb
pol-cova <83374367+pol-cova@users.noreply.github.com>
2023-03-30 15:00:29 -0600

    Calculadora  commit

--- main.c
+++ main.c
@@ -2,5 +2,20 @@
 // Conjunto de lenguaje Sintaxis
 int main() {
     printf("Hello, World!\n");
+
+
+    int i =0, a = 5, b = 5;
+
+    while(i <6)
+
+    {
+
+        a+=b;
+
+        i++;
+
+    }
+    printf("%d", i);
+
     return 0;
 }
